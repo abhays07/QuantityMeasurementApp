8c5985c974fd0b1faab88e1b45b066263d19c6c5
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-04-16 10:14:05 +0530

    [Abhay Singh Lodhi] : edited securityconfig and pom.xml for api-gateway.

--- api-gateway/src/main/java/com/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/apigateway/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
+import java.util.List;
 import java.util.Arrays;
 
 @Configuration
